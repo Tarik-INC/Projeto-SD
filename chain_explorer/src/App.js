019b133da163e895e69218531664a9c4f7656fda
Tarik <tarikesmin@gmail.com>
2018-12-04 01:40:01 -0200

    Adição da mineração por broadcast, nova rota para obter a lista de nos adicionada no app.py, erros de lógica e correções gramaticais no código

--- chain_explorer/src/App.js
+++ chain_explorer/src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import API from './utils/api'
 import axios from 'axios'
-import {PacmanLoader } from 'react-spinners';
-import {Button} from 'reactstrap';
+import { PacmanLoader } from 'react-spinners';
+import { Button } from 'reactstrap';
 import './App.css';
 import Transactions from './Components/transactions/Transactions';
 
 class App extends Component {
-  
+
   state = {
-    requestError : null,
-    minedBlocks : [],
+    requestError: null,
+    minedBlocks: [],
     loading: true,
     isMining: false,
+    nodes: [],
   };
-  
+
   componentDidMount() {
-  
+
+    API.get('nodes')
+      .then(response => response.data)
+      .then(data => this.setState({ nodes: data }))
+      .catch(err => {
+        this.setState({ requestError: err })
+      })
+
     API.get('chain')
-    .then(response => response.data )
-    .then(data => this.setState({minedBlocks: data.chain, loading:false}))
-    .catch(err => {
-      this.setState({requestError: err})
-    })
+      .then(response => response.data)
+      .then(data => this.setState({ minedBlocks: data.chain, loading: false }))
+      .catch(err => {
+        this.setState({ requestError: err })
+      })
   };
 
   refreshHandler = () => {
@@ -34,37 +42,48 @@ class App extends Component {
       }
     })
 
-   setTimeout(() => {
-     API.get('chain')
-       .then(response => response.data)
-       .then(data => this.setState((state) => {
-         return {
-           ...state,
-           minedBlocks: data.chain,
-           loading: false,
-         }
-       }))
-       .catch(err => {
-         this.setState({ requestError: err })
-       })
-   }, 3000);
-    
+    setTimeout(() => {
+      API.get('chain')
+        .then(response => response.data)
+        .then(data => this.setState((state) => {
+          return {
+            ...state,
+            minedBlocks: data.chain,
+            loading: false,
+          }
+        }))
+        .catch(err => {
+          this.setState({ requestError: err })
+        })
+    }, 3000);
+
   }
 
   miningHandler = () => {
 
-    this.setState({isMining: true})
-    
-    axios.get('http://localhost:5000/mine')
-    .then(response => response.data)
-    .then( data => this.refreshHandler())
+    this.setState({ isMining: true })
 
+
+    axios.get("http://localhost:5000/mine")
+      .then(response => response.data)
+      .then(data => this.refreshHandler())
+      .catch(err =>
+        this.setState({ requestError: err }))
+
+    this.state.nodes.forEach((num, index) => {
+      let url_string = "http://" + num + "/mine"
+      axios.get(url_string)
+        .then(response => response.data)
+        .then(data => this.refreshHandler())
+        .catch(err =>
+          this.setState({ requestError: err }))
+    })
 
     setTimeout(() => {
-      this.setState({isMining: false})
-    }, 3000);
+      this.setState({ isMining: false })
+    }, 4000);
 
-   
+    this.refreshHandler()
   }
 
   render() {
@@ -74,28 +93,29 @@ class App extends Component {
     if (this.state.loading === false) {
       displayResult = (
         this.state.minedBlocks.map((item, index) => (
-          <div className='block'>
-            <h1> Bloco {item.index === 1 ?  'Gênesis': item.index  } </h1>
+          <div className='block' key={index}>
+            <h1> Bloco {item.index === 1 ? 'Gênesis' : item.index} </h1>
             <div>
               <Transactions data={item.transactions} />
             </div>
           </div>
         )
+        )
       )
-      )}
+    }
     else {
       displayResult = (
-      <div className='icon_container'>
-          <PacmanLoader color='#ff9900'   className='loading_icon'/>
-      </div>
+        <div className='icon_container'>
+          <PacmanLoader color='#ff9900' className='loading_icon' />
+        </div>
       )
     }
 
     return (
       <div className='container'>
-        <Button disabled={this.state.isMining}  ref='mining_btn' color='warning' size='lg' onClick={this.miningHandler} className='mining_button'> {this.state.isMining? 'Minerando...': 'Minerar'} </Button>
-        <Button color='primary' size = 'lg'  className='refresh_button' onClick={this.refreshHandler}> Atualizar </Button>
-        
+        <Button disabled={this.state.isMining} ref='mining_btn' color='warning' size='lg' onClick={this.miningHandler} className='mining_button'> {this.state.isMining ? 'Minerando...' : 'Minerar'} </Button>
+        <Button color='primary' size='lg' className='refresh_button' onClick={this.refreshHandler}> Atualizar </Button>
+
         {displayResult}
         {/* {this.state.minedBlocks.map((item, index) => (
           <div>
@@ -106,7 +126,7 @@ class App extends Component {
               <Transactions data={item.transactions}/>
             </div>
           </div> */}
-      </div>  
+      </div>
     );
   }
 }
